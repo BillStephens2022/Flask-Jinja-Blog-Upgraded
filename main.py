715e23731912e00e29df92e33b77b7508619747b
BillStephens2022 <two4onebill@yahoo.com>
2023-04-21 14:57:54 -0400

    set up single post page and linked up route

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import requests
 
 app = Flask(__name__)
 
-posts = requests.get("https://api.npoint.io/ef76f82bb6da04f755f7").json()
-print(posts)
+posts = requests.get("https://api.npoint.io/7d9e5abc21f62bbcef83").json()
+
 
 @app.route("/")
 def get_all_posts():
@@ -20,6 +20,15 @@ def about():
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 
 if __name__ == "__main__":
